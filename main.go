e2bda80297fd461e39d61e578b57e69f7d020c84
Jonathan Bowe <bowejonathan99@gmail.com>
2025-04-06 13:57:26 -0400

    Working but slow

--- main.go
+++ main.go
@@ -9,57 +9,66 @@ import (
 	"strings"
 )
 
-type CityResults struct {
-	Name string
-	Min  float64
-	Mean float64
-	Max  float64
+// Remove name, add NumRecords
+type CityRecords struct {
+	NumRecords float64
+	Min        float64
+	Mean       float64
+	Max        float64
+}
+
+func updateCity(m map[string]CityRecords, name string, temp float64) {
+	if records, ok := m[name]; ok {
+		if temp < records.Min {
+			records.Min = temp
+		}
+		if temp > records.Max {
+			records.Max = temp
+		}
+		records.Mean += temp
+		records.NumRecords += 1.0
+	} else {
+		m[name] = CityRecords{
+			NumRecords: 1.0,
+			Min:        temp,
+			Mean:       temp,
+			Max:        temp,
+		}
+	}
+
 }
 
 func main() {
-	cities := map[string][]float64{}
+	cityTemperatures := map[string]CityRecords{}
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		text := strings.Split(scanner.Text(), ";")
+		name := text[0]
 		temp, err := strconv.ParseFloat(text[1], 64)
 		if err != nil {
 			fmt.Printf("ERROR converting float: %v", err)
 			os.Exit(1)
 		}
-		cities[text[0]] = append(cities[text[0]], temp)
-	}
-	cityResults := []CityResults{}
-	for city, temps := range cities {
-		var min, sum, max float64 = 0.0, 0.0, 0.0
-		for i, temp := range temps {
-			if i == 0 {
-				min = temp
-			} else if temp < min {
-				min = temp
-			}
-			if temp > max {
-				max = temp
-			}
-			sum += temp
-		}
-		cityResults = append(cityResults, CityResults{
-			Name: city,
-			Min:  min,
-			Mean: sum / float64(len(temps)),
-			Max:  max,
-		})
+		//Race condition for concurrent writes to map
+		updateCity(cityTemperatures, name, temp)
 	}
 
-	slices.SortStableFunc(cityResults, func(i, j CityResults) int {
-		return strings.Compare(i.Name, j.Name)
-	})
+	cityNames := make([]string, 0, len(cityTemperatures))
+	for name, records := range cityTemperatures {
+		cityNames = append(cityNames, name)
+		records.Mean = records.Mean / records.NumRecords
+	}
+
+	slices.Sort(cityNames)
 	fmt.Print("{")
-	for i, v := range cityResults {
+	for i, name := range cityNames {
+		records := cityTemperatures[name]
 		//Expected output: Abha=-23.0/18.0/59.2
-		fmt.Printf("%s=%.1f/%.1f/%.1f", v.Name, v.Min, v.Mean, v.Max)
-		if i != len(cityResults)-1 {
+		fmt.Printf("%s=%.1f/%.1f/%.1f", name, records.Min, records.Mean, records.Max)
+		if i < len(cityNames)-1 {
 			fmt.Print(", ")
 		}
+
 	}
 	fmt.Print("}\n")
 }
